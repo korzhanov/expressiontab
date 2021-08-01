493a541ca86157dbb2df489c557ec289b82401cc
Oleg Korzhanov <ok@xfactorial.com>
2021-08-02 00:15:56 +0300

    Two Timer modes

--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -2,13 +2,25 @@
     import { onMount, onDestroy } from "svelte";
     import { fly } from "svelte/transition";
 
-    
     let timerType: number = 0;
+    let hour: string = "";
+    let min: string = "";
+    let sec: string = "";
     let date: any = new Date();
     let interval: any;
-    $: hour = ("0" + (23 - date.getHours())).slice(-2);
-    $: min = ("0" + (59 - date.getMinutes())).slice(-2);
-    $: sec = ("0" + (59 - date.getSeconds())).slice(-2);
+    $: switch (timerType) {
+        case 0:
+            hour = ("0" + (23 - date.getHours())).slice(-2);
+            min = ("0" + (59 - date.getMinutes())).slice(-2);
+            sec = ("0" + (59 - date.getSeconds())).slice(-2);
+
+            break;
+        case 1:
+            hour = ("0" + date.getHours()).slice(-2);
+            min = ("0" + date.getMinutes()).slice(-2);
+            sec = ("0" + date.getSeconds()).slice(-2);
+            break;
+    }
 
     onMount(() => {
         interval = setInterval(() => {
@@ -20,7 +32,7 @@
     });
 </script>
 
-<section class="clock-and-greeting">
+<clock-and-greeting on:click="{()=>{timerType+1>1?timerType=0:timerType++}}">
     <!-- <div class="user-greeting">
       <h1 id="user-greeting-time">Good evening,&nbsp;</h1>
       <h1 id="user-greeting-name">what's your <button>name</button>?</h1>
@@ -30,15 +42,14 @@
       </form>
     </div> -->
 
-    <div class="time-display">
+    <time-display>
         <p>
-            {#key hour}<span in:fly={{ y: -20 }} >{hour}</span>{/key}:
-            {#key min}<span in:fly={{ y: -20 }} >{min}</span>{/key}:
-            {#key sec}<span in:fly={{ y: -20 }} >{sec}</span> {/key}
+            {#key hour}<span in:fly={{ y: -20 }}>{hour}</span>{/key}:
+            {#key min}<span in:fly={{ y: -20 }}>{min}</span>{/key}:
+            {#key sec}<span in:fly={{ y: -20 }}>{sec}</span> {/key}
             <!-- <span>:{msec}</span> -->
         </p>
-       
-    </div>
+    </time-display>
 
     <!-- <div class="toggle-pomodoro">
       <button class="start" style="display: none;">Start Pomodoro Cycle</button>
@@ -47,40 +58,40 @@
       <button class="reset" style="display: inline-block;">Reset</button>
       <button class="work-break" style="display: inline-block;">Break</button>
     </div> -->
-</section>
+</clock-and-greeting>
 
 <style lang="postcss">
-    .clock-and-greeting {
+    clock-and-greeting {
         height: 75%;
         flex-direction: column;
         /* font-family: Monoton, Lato, sans-serif; */
         font-family: Lato, sans-serif;
     }
-    .clock-and-greeting,
+    clock-and-greeting,
     .user-greeting {
         display: flex;
         justify-content: center;
     }
-    .time-display {
+    time-display {
         display: flex;
         justify-content: center;
         font-weight: 300;
         color: #fff;
         font-size: 12vw;
     }
-    .time-display,
-    .time-display p {
+    time-display,
+    time-display p {
         padding: 0;
         margin: 0;
         /* transition: all 0.3s ease; */
         text-shadow: 0 0 50px #0000009d;
     }
-    .time-display p:hover {
+    time-display p:hover {
         /* transform: scale(1.03); */
         transition: all 0.3s ease;
         /* text-shadow: 0 0 50px #0000009d; */
     }
-    .time-display p span {
+    time-display p span {
         width: 1.5em;
         display: inline-block;
         transition: all 0.3s ease;
