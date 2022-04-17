495cbfb19220c8c64f25268faaf721998b2a1d3c
Oleg Korzhanov <ok@xfactorial.com>
2022-04-17 11:17:24 +0400

    vite config pre prod

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { chromeExtension, simpleReloader } from "vite-plugin-chrome-extension";
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 const mode = "development"; // production // development
 const isProduction = mode === 'production'; // boolean
@@ -9,23 +10,57 @@ const isProduction = mode === 'production'; // boolean
 // https://vitejs.dev/config/
 export default defineConfig({
   mode: mode,
-  optimizeDeps: {
-    include: ["svelte-hero-icons"],
-  },
+  // optimizeDeps: {
+  //   include: ["svelte-hero-icons"],
+  // },
   logLevel: isProduction ? "silent" : "info",
   // logLevel: "info",
   plugins: [
     svelte({
-      emitCss: isProduction,
+      // emitCss: isProduction,
+      emitCss: true,
+    }),
+    createHtmlPlugin({
+      // minify: true,
+      // pages : [ 
+      //   {
+      // entry: "src/newtab/index.js",
+      // template: "src/newtab/index.html",
+      inject: {
+        // data: {
+        //   injectStylesheet: `<link rel="stylesheet" type="text/css" href="main.css" />`,
+        //   injectScript: `<script type="module" src="main.js"></script>`,
+        // },
+        // tags: [
+        //   {
+        //     injectTo: 'head',
+        //     tag: 'link',
+        //     attrs: {
+        //       href: 'main.css',
+        //       rel: "stylesheet",
+        //       type: "text/css"
+        //     },
+        //   },
+        //   {
+        //     injectTo: 'body',
+        //     tag: 'script',
+        //     attrs: {
+        //       src: 'main.js',
+        //       type: "module"
+        //     },
+        //   },
+        // ],
+      },
+      // }],
     }),
     chromeExtension(
       {
-        verbose: true,
-        dynamicImportWrapper: false,
-        contentScriptWrapper: false,
+        // verbose: true,
+        // dynamicImportWrapper: true,
+        // contentScriptWrapper: false,
       }
     ),
-        // {
+    // {
     //   name: 'disable-treeshake',
     //   transform(code, id) {
     //     if (id.endsWith('.html')) {
@@ -35,7 +70,7 @@ export default defineConfig({
     //   },
     // },
 
-    simpleReloader(),
+    // simpleReloader(),
   ],
   resolve: {
     alias: {
@@ -56,14 +91,17 @@ export default defineConfig({
         assetFileNames: "[name].[ext]"
       },
     },
-    watch: {
-      buildDelay: 2000,
-      clearScreen: true,
-      include: __dirname + "/src/**",
-    },
+    // watch: {
+    //   buildDelay: 1000,
+    //   chokidar: true,
+    //   clearScreen: false,
+    //   include: resolve(__dirname + "/src/**"),
+    //   skipWrite: true,
+    // },
     outDir: "builds/expressiontab",
+    external: true,
+    // sourcemap: 'inline',
+    assetsInlineLimit: 0,
+    minify: 'terser',
   },
-  sourcemap: 'inline',
-  assetsInlineLimit: 0,
-  minify: 'terser',
 });
