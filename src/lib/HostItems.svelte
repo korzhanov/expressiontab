605c3048c3e708bbd1048b0d3114a8a3c95804f0
Oleg Korzhanov <ok@xfactorial.com>
2022-04-25 15:23:59 +0400

    add middle hostitems

---
+++ src/lib/HostItems.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import HostItem from "./HostItem.svelte";
+
+  export let value: any;
+  export let key: any;
+//   export let width: any;
+  console.log("!!! hostItems !!! key", key);
+//   console.log("!!! hostItems !!! width", width);
+//   console.log("!!! hostItems !!! value", value);
+</script>
+
+{#each value as hostItem (hostItem)}
+  <HostItem {hostItem} {key}/>
+{/each}
