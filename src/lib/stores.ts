bfc66f60db828b404bf2d30aac0f4abf537273c8
Oleg Korzhanov <ok@xfactorial.com>
2022-04-26 22:26:31 +0400

    add nodesList in store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -4,3 +4,4 @@ import { writable } from "svelte/store";
 export const filteredListSliced = persist(writable([]),indexedDBStorage(),'filteredListSliced');
 // export const chankList = writable([]);
 export const favicons = writable(new Map());
+export const nodesList = persist(writable([]),indexedDBStorage(),'nodesList');
